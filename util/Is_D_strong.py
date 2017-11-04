03def16c57e7c232de3918abcd3aeb2e3a234e33
Atsushi Kayama <a.shika829@gmail.com>
2017-11-04 21:40:56 +0900

    change Is_D_strong

--- util/Is_D_strong.py
+++ util/Is_D_strong.py
@@ -11,15 +11,16 @@ def Is_D_strong(real_output, syn_output, id_label_tensor, pose_label_tensor, syn
     # Discriminator の正解率を算出し， 指定した以上の正解率であれば， 十分強いとみなす
 
     """
-    _, id_real_ans = torch.max(real_output[:, :Nd+1], 1)
+    _, id_real_ans = torch.max(real_output[:, :Nd], 1)
     _, pose_real_ans = torch.max(real_output[:, Nd+1:], 1)
-    _, id_syn_ans = torch.max(syn_output[:, :Nd+1], 1)
+    _, id_syn_ans = torch.max(syn_output[:, :Nd], 1)
 
     id_real_precision = (id_real_ans==id_label_tensor).type(torch.FloatTensor).sum() / real_output.size()[0]
     pose_real_precision = (pose_real_ans==pose_label_tensor).type(torch.FloatTensor).sum() / real_output.size()[0]
-    id_syn_precision = (id_syn_ans==syn_id_label_tensor).type(torch.FloatTensor).sum() / syn_output.size()[0]
+    gan_real_precision = (real_output[:,Nd].sigmoid()>=0.5).type(torch.FloatTensor).sum() / real_output.size()[0]
+    gan_syn_precision = (syn_output[:,Nd].sigmoid()<0.5).type(torch.FloatTensor).sum() / syn_output.size()[0]
 
-    total_precision = (id_real_precision+pose_real_precision+id_syn_precision)/3
+    total_precision = (id_real_precision+pose_real_precision+gan_real_precision+gan_syn_precision)/4
 
     # Variable(FloatTensor) -> Float へと変換
     total_precision = total_precision.data[0]
